7dae0d718b748a61d55f914596d8d832d3e4f697
heidsoft <heidsoft@qq.com>
2020-02-21 14:41:10 +0800

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,9 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>;
+//   }
+// }
+
+// const element = <h1>Hello, world</h1>;
+
+function Welcome(props) {
+  console.log(props)
+  return <h1>Hello, {props.name}</h1>;
+}
+
+
+
+function tick() {
+  const element = <Welcome name="Sara" />;
+
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+
+// ReactDOM.render(element, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
