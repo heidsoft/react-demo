982aed629f14e16e5898b69fb967bf8bb712b164
heidsoft <heidsoft@qq.com>
2020-02-27 10:48:30 +0800

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // class Welcome extends React.Component {
@@ -17,24 +16,53 @@ function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
-function Jake() {
-  return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
-    </div>
-  );
+function FormattedDate(props) {
+  return  <h2>It is {props.date.toLocaleTimeString()}.</h2>
+}
+
+// 状态测试
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+      console.log("componentDidMount ...start");
+      this.timeID = setInterval(
+        ()=> this.tick(),
+        1000
+      );
+       console.log("componentDidMount ...end");
+  }
+
+  componentWillUnmount() {
+      console.log("componentWillUnmount ...");
+      clearInterval(this.timeID)
+  }
+
+  tick() {
+  	this.setState({
+  		date: new Date()
+  	});
+  }
+
+  // 采用组建格式化
+  render() {
+    return (
+      <div>
+        <h1>this is my first clock react app!</h1>
+        <FormattedDate date={this.state.date} />
+      </div>
+    );
+  }
 }
 
 
-function tick() {
-  const element = <Jake />;
 
-  ReactDOM.render(element, document.getElementById('root'));
-}
 
-setInterval(tick, 1000);
+const element = <FormattedDate />;
+  
+ReactDOM.render(element, document.getElementById('root'));
 
-// ReactDOM.render(element, document.getElementById('root'));
 
